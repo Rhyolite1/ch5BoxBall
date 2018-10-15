7c25085958b61894d266a67f58c26b9104a4d584
Rhyolite1 <43154700+Rhyolite1@users.noreply.github.com>
2018-10-15 15:11:03 -0400

    Project Complete

---
+++ BoxBall.java
@@ -0,0 +1,141 @@
+import java.awt.*;
+import java.awt.geom.*;
+/**
+ * Write a description of class BoxBall here.
+ * 
+ * @author  Michael Moretti
+ * @version 2018 October 15
+ *
+ */
+public class BoxBall
+{
+    private static final int GRAVITY = 1; 
+
+    private int ballDegradation = 2;
+    private Ellipse2D.Double circle;
+    private Color color;
+    private int diameter;
+    private int xPosition;
+    private int yPosition;
+    private final int groundPosition;
+    //y position of ceiling
+    private final int ceilingPosition;
+    //x position of left wall
+    private final int leftWallPosition;
+    //x position of right wall
+    private final int rightWallPosition;
+    private Canvas canvas;
+    private int xSpeed;
+    private int ySpeed;
+
+    /**
+     * Constructor for objects of class BouncingBall
+     *
+     * @param xPos  the horizontal coordinate of the ball
+     * @param yPos  the vertical coordinate of the ball
+     * @param ballDiameter  the diameter (in pixels) of the ball
+     * @param ballColor  the color of the ball
+     * @param groundPos  the position of the ground (where the wall will bounce)
+     * @param ceilingPos  the position of the ceiling
+     * @param leftPos  the position of the left wall
+     * @param rightPos  the position of the right wall
+     * @param drawingCanvas  the canvas to draw this ball on
+     */
+    public BoxBall(int xPos, int yPos, int ballDiameter, Color ballColor,
+                        int groundPos, int ceilingPos, int leftPos, int rightPos, Canvas drawingCanvas,
+                        int horSpeed, int verSpeed)
+    {
+        xPosition = xPos;
+        yPosition = yPos;
+        color = ballColor;
+        diameter = ballDiameter;
+        groundPosition = groundPos;
+        ceilingPosition = ceilingPos;
+        leftWallPosition = leftPos;
+        rightWallPosition = rightPos;
+        canvas = drawingCanvas;
+        xSpeed = horSpeed;
+        ySpeed = verSpeed;
+    }
+
+    /**
+     * Draw this ball at its current position onto the canvas.
+     **/
+    public void draw()
+    {
+        canvas.setForegroundColor(color);
+        canvas.fillCircle(xPosition, yPosition, diameter);
+    }
+
+    /**
+     * Erase this ball at its current position.
+     **/
+    public void erase()
+    {
+        canvas.eraseCircle(xPosition, yPosition, diameter);
+    }    
+
+    /**
+     * Move this ball according to its position and speed and redraw.
+     **/
+    public void move()
+    {
+        // remove from canvas at the current position
+       erase();
+            
+        // compute new position
+       yPosition += ySpeed;
+       xPosition += xSpeed;
+
+        //check if ball(s) has hit something
+       if(yPosition >= (groundPosition - diameter) && ySpeed > 0)
+       {
+            yPosition = (groundPosition - diameter);
+            ySpeed = -ySpeed;
+       }
+        
+       else if (yPosition <= (ceilingPosition) && ySpeed < 0)
+       {
+            yPosition = ceilingPosition;
+            ySpeed = -ySpeed;
+       }
+        
+       else if(xPosition <= (leftWallPosition) && xSpeed < 0)
+       {
+           xPosition = leftWallPosition;
+           xSpeed = -xSpeed;
+       }
+        
+       else if(xPosition >= (rightWallPosition - diameter) && xSpeed > 0)
+       {
+            xPosition = (rightWallPosition - diameter);
+            xSpeed = -xSpeed;
+       }
+       else
+       {
+            ySpeed = ySpeed;
+            xSpeed = xSpeed;
+            yPosition = yPosition;
+            xPosition = xPosition;
+       }
+
+       draw();
+    }
+    
+    /**
+     * return the horizontal position of this ball
+     */
+    public int getXPosition()
+    {
+        return xPosition;
+    }
+
+    /**
+     * return the vertical position of this ball
+     */
+    public int getYPosition()
+    {
+        return yPosition;
+    }
+   
+}
